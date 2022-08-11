8650f6439374613dc42481141ef7b2ae763c5320
clayang3 <claire.xl.yang@gmail.com>
2022-08-10 21:39:46 -0700

    finished project and education sections

--- app.js
+++ app.js
@@ -15,11 +15,9 @@ function openMediaScroller() {
     let media = document.getElementById("media-scroller");
     if (media.classList.contains("reveal")) {
         media.classList.remove("reveal");
-        console.log("hide");
     }
     else {
         media.classList.add("reveal");
-        console.log("show");
     }
 }
 
