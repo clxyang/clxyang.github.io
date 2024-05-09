beb4c0f9c3aa76cfa38f28bf4201f5244189349e
clayang3 <claire.xl.yang@gmail.com>
2024-05-09 17:13:43 -0400

    refactoring + remove insta

---
+++ 2022/README.md
@@ -0,0 +1,1 @@
+Repo contains an archive of old portfolio work, current portfolio progress, and my CS1300 writeups 😸
---
+++ 2024/app.js
@@ -0,0 +1,125 @@
+function openBlurb(name) {
+    let blurb = document.getElementById(name + "-blurb");
+    let item = document.getElementById(name + "-item");
+    if (blurb.classList.contains("reveal")) {
+        blurb.classList.remove("reveal");
+        item.classList.remove("reveal-margin");
+    }
+    else {
+        blurb.classList.add("reveal");
+        item.classList.add("reveal-margin");
+    }
+}
+
+function openMediaScroller() {
+    let media = document.getElementById("media-scroller");
+    if (media.classList.contains("reveal")) {
+        media.classList.remove("reveal");
+    }
+    else {
+        media.classList.add("reveal");
+    }
+}
+
+let lastScrollY = window.scrollY;
+
+window.onscroll = (event) => {
+    toggleNavBar();
+}
+
+function toggleNavBar() {
+    if (window.scrollY > 25 && lastScrollY < window.scrollY) {
+        document.getElementById("navbar").classList.add("hidden");
+    } 
+    else {
+        document.getElementById("navbar").classList.remove("hidden");
+    }
+
+    lastScrollY = window.scrollY;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const theWords = ["making choreo for my dance team", "studying CS at Brown", "playing the NYT Strands"];
+    let theBox = document.getElementById("word");
+    console.log(theBox);
+    let idx = 0;
+
+    const writeBox = (word) => {
+        const pieces = word.split("");
+        let letterIndex = 0;
+        let isDeleting = false;
+    
+        const writeLetters = () => {
+            if (isDeleting) {
+                if (letterIndex >= 0) {
+                    theBox.innerHTML = pieces.slice(0, letterIndex).join("") + "<span class='cursor'></span>";
+                    letterIndex--;
+                    setTimeout(writeLetters, 80); // Adjust the timeout for deletion speed
+                } else {
+                    isDeleting = false;
+                    switchWord();
+                }
+            } else {
+                if (letterIndex <= pieces.length) {
+                    theBox.innerHTML = pieces.slice(0, letterIndex).join("") + "<span class='cursor'></span>";
+                    letterIndex++;
+                    setTimeout(writeLetters, 80); // Adjust the timeout for typing speed
+                } else {
+                    isDeleting = true;
+                    setTimeout(writeLetters, 2000); // Adjust the timeout before starting deletion
+                }
+            }
+        };
+    
+        writeLetters();
+    };
+    const switchWord = () => {
+        idx = idx >= theWords.length - 1 ? 0 : idx + 1;
+        writeBox(theWords[idx]);
+    };
+
+    switchWord();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const theWords = ["making choreo for my dance team", "studying CS at Brown", "playing the NYT Strands"];
+    let theBox = document.getElementById("mobile-word");
+    console.log(theBox);
+    let idx = 0;
+
+    const writeBox = (word) => {
+        const pieces = word.split("");
+        let letterIndex = 0;
+        let isDeleting = false;
+    
+        const writeLetters = () => {
+            if (isDeleting) {
+                if (letterIndex >= 0) {
+                    theBox.innerHTML = pieces.slice(0, letterIndex).join("") + "<span class='cursor'></span>";
+                    letterIndex--;
+                    setTimeout(writeLetters, 80); // Adjust the timeout for deletion speed
+                } else {
+                    isDeleting = false;
+                    switchWord();
+                }
+            } else {
+                if (letterIndex <= pieces.length) {
+                    theBox.innerHTML = pieces.slice(0, letterIndex).join("") + "<span class='cursor'></span>";
+                    letterIndex++;
+                    setTimeout(writeLetters, 80); // Adjust the timeout for typing speed
+                } else {
+                    isDeleting = true;
+                    setTimeout(writeLetters, 2000); // Adjust the timeout before starting deletion
+                }
+            }
+        };
+    
+        writeLetters();
+    };
+    const switchWord = () => {
+        idx = idx >= theWords.length - 1 ? 0 : idx + 1;
+        writeBox(theWords[idx]);
+    };
+
+    switchWord();
+});
