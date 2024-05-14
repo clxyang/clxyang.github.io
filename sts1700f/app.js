6fbe4f9f0e5fff7d8759a3c6c887ff871113fcab
clayang3 <claire.xl.yang@gmail.com>
2024-05-14 00:28:17 -0400

    final site

--- sts1700f/app.js
+++ sts1700f/app.js
@@ -1,8 +1,24 @@
 function openBlurb(name) {
     // let blurb = document.getElementById(name + "-blurb");
     let item = document.getElementById(name + "-item");
+    // candle=barrett, flowers=wallace, chair=wallace, book=dalbe
+    let person = "wallace"
+    if (name == "candle") {
+        person = "barrett";
+    }
+    if (name == "book") {
+        person = "dalbe";
+    }
+    if (name == "chair") {
+        person = "chair"
+    }
+    // console.log("person is ", person);
+
+    let quote = document.getElementById(person + "-q")
+
     if (item.classList.contains("reveal")) {
         item.classList.remove("reveal");
+        quote.classList.remove("side");
     }
     else {
         candle = document.getElementById("candle-item");
@@ -10,20 +26,52 @@ function openBlurb(name) {
         book = document.getElementById("book-item");
         chair = document.getElementById("chair-item");
 
+        wallace = document.getElementById("wallace-q");
+        barrett = document.getElementById("barrett-q")
+        dalbe = document.getElementById("dalbe-q")
+        chairq = document.getElementById("chair-q")
+
         if (candle.classList.contains("reveal")) {
             candle.classList.remove("reveal");
+            barrett.classList.remove("side");
+            barrett.classList.add("hide");
         }
         if (flowers.classList.contains("reveal")) {
             flowers.classList.remove("reveal");
+            wallace.classList.remove("side");
+            wallace.classList.add("hide");
         }
         if (book.classList.contains("reveal")) {
             book.classList.remove("reveal");
+            dalbe.classList.remove("side");
+            dalbe.classList.add("hide");
         }
         if (chair.classList.contains("reveal")) {
             chair.classList.remove("reveal");
+            chair.classList.remove("side");
+            chairq.classList.add("hide");
         }
 
+        // if (wallace.classList.contains("side")) {
+        //     wallace.classList.remove("side");
+        // }
+        // if (barrett.classList.contains("side")) {
+        //     barrett.classList.remove("side");
+        // }
+        // if (dalbe.classList.contains("side")) {
+        //     dalbe.classList.remove("side");
+        // }
+
+        wallace.classList.add("hide");
+        barrett.classList.add("hide");
+        dalbe.classList.add("hide");
+        chairq.classList.add("hide");
+
+        if (quote.classList.contains("hide")) {
+            quote.classList.remove("hide");
+        }
         item.classList.add("reveal");
+        quote.classList.add("side");
     }
 }
 
